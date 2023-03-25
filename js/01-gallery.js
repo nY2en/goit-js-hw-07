570e48fbb1b61610fa8a3c286b4e1dc236daf701
Nikita Chebanenko <chebanikita@icloud.com>
2023-03-25 20:23:40 +0200

    зробив перше завдання

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -2,3 +2,42 @@ import { galleryItems } from './gallery-items.js';
 // Change code below this line
 
 console.log(galleryItems);
+
+const gallery = document.querySelector('.gallery');
+
+function createGalleryMarkup(pictures) {
+    return pictures
+        .map(
+            picture =>
+                `<li class="gallery__item"><a "href="${picture.original}" class="gallery__link"><img src="${picture.preview}" data-source="${picture.original}" alt="${picture.description}" class="gallery__image"/></a></li>`
+        )
+        .join('');
+}
+
+gallery.innerHTML = createGalleryMarkup(galleryItems);
+
+gallery.addEventListener('click', onGalleryChange);
+
+function onGalleryChange(e) {
+    e.preventDefault();
+
+    const pictures = [...gallery.querySelectorAll('.gallery__image')];
+
+    if (!pictures.includes(e.target)) {
+        return;
+    }
+
+    const instance = basicLightbox.create(`
+    <img src="${e.target.dataset.source}">
+`);
+    instance.show();
+    window.addEventListener('keydown', onKeyChange);
+    function onKeyChange(e) {
+        const ESCAPE = 'Escape';
+
+        if (e.code === ESCAPE) {
+            instance.close();
+            window.removeEventListener('keydown', onKeyChange);
+        }
+    }
+}
